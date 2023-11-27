948691336cc2d77edc8671855249bd1b6a102e11
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-27 15:28:17 -0500

    cool

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,15 +16,17 @@ export default function Home() {
       <Divider section='3'/>
       <div className="alien">
         <h2>Alien Planets</h2>
+        <div className='container'>
         {alienDetails.map((otherPlanets)=>{
           return (
-          <div className='alienCard' key={otherPlanets.id}>
+            <div className='alienCard' key={otherPlanets.id}>
             <p>{ otherPlanets.emoji } </p>
             <p>{ otherPlanets.name } </p>
             <p>{ otherPlanets.name }  is { otherPlanets.distanceFromEarth } from earth. { otherPlanets.name } is {otherPlanets.hasLife ? "believed to have life" : "believed to not have life"}. The atmosphere of {otherPlanets.name} is {otherPlanets.atmosphere}.</p>
           </div>
         )
         })}
+        </div>
       </div>
     </main>
   )
