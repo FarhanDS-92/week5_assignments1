0559f4e49fae2de171df7038c9f0431aec4a03d1
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-11-27 14:55:01 -0500

    change

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+// import Image from 'next/image'
+"use Client"
+import alienPlanets from "@/lib/aliens.js"
+import { useState } from "react"
+
+export default function Home() {
+  return (
+    <main>
+
+    </main>
+  )
+}
